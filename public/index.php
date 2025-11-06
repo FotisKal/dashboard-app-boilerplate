6b5376b26c59b0066d0903727f598af32b0c2c9a
Fotis Kal <fotis_kalokiris@outlook.com>
2025-11-06 22:45:23 +0200

    feat: add vlucas/phpdotenv

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Controllers\HomeController;
+use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
@@ -11,6 +12,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 define('STORAGE_PATH', __DIR__ . '/../storage');
 define('VIEW_PATH', __DIR__ . '/../views');
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 $app = AppFactory::create();
 
