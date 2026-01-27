a8c19fff769d66f1591c3ef883e9e2905844d44b
Fotis Kal <fotis_kalokiris@outlook.com>
2026-01-27 22:41:34 +0200

    refactor: move migration commands into my-cli-app

--- my-cli-app.php
+++ my-cli-app.php
@@ -2,6 +2,24 @@
 
 declare(strict_types=1);
 
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\Migration\PhpFile;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Tools\Console\Command\{
+    CurrentCommand,
+    DiffCommand,
+    DumpSchemaCommand,
+    ExecuteCommand,
+    GenerateCommand,
+    LatestCommand,
+    ListCommand,
+    MigrateCommand,
+    RollupCommand,
+    StatusCommand,
+    SyncMetadataCommand,
+    UpToDateCommand,
+    VersionCommand
+};
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
@@ -13,9 +31,25 @@ $container = $app->getContainer();
 // replace with mechanism to retrieve EntityManager in your app
 $entityManager = $container->get(EntityManager::class);
 
+$config            = new PhpFile(CONFIG_PATH . '/migrations.php');
+$dependencyFactory = DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
+
 $commands = [
     // If you want to add your own custom console commands,
     // you can do so here.
+    new CurrentCommand($dependencyFactory),
+    new DumpSchemaCommand($dependencyFactory),
+    new ExecuteCommand($dependencyFactory),
+    new GenerateCommand($dependencyFactory),
+    new LatestCommand($dependencyFactory),
+    new MigrateCommand($dependencyFactory),
+    new RollupCommand($dependencyFactory),
+    new StatusCommand($dependencyFactory),
+    new VersionCommand($dependencyFactory),
+    new UpToDateCommand($dependencyFactory),
+    new SyncMetadataCommand($dependencyFactory),
+    new ListCommand($dependencyFactory),
+    new DiffCommand($dependencyFactory),
 ];
 
 ConsoleRunner::run(
